0ead1cedde254b7d2fa76dc73867b9ddc30297a3
Camila Oyene <coyene63@gmail.com>
2025-02-25 03:55:02 -0300

    index, funcion crearTarjetaMovie en proceso

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,44 @@
 console.log(tempData);
+import tempData from './tempData.js';
+
+const tempData = tempData;
+
+function crearTarjetaMovie(tempData) {
+    const {title, year, director, duration, genre, rate, poster} = tempData;
+    
+    //Crear elementos HTML:
+    //crear tarjeta = div
+    const tarjetaMovie = document.createElement('div');
+    //Asignar el valor correspondiente a la propiedad del elemento
+    tarjetaMovie.classList.add('tarjetaMovie');
+
+    //title = h3
+    const titulo = document.createElement('h3')
+    titulo.textContent = title;
+
+    //year = 
+    const año = document.createElement('p');
+    año.textContent = year
+
+    //director = 
+    const directorMovie = document.createElement('p');
+    directorMovie.textContent = director
+   
+    //duration =
+    const duracion = document.createElement('p');
+    duracion.textContent = duration
+
+    //genre =  
+    const genero = document.createElement('p');
+    genero.textContent = genre
+
+    //rate =
+    const puntuacion = document.createElement('p');
+    puntuacion.textContent = rate
+    
+    //poster =
+    const imagenMovie = document.createElement('img');
+    imagenMovie.src = poster
+    imagenMovie.alt = title
+    
+}
